3145fd8b216eb4d0fc44c27677f9ee459de04e6f
Immanuel Lam <immanuel.c.lam@student.uts.edu.au>
2026-04-22 14:49:48 +1000

    feat: inject AppThemeViewModel into root view (#12)

--- ewbapp/ewbappApp.swift
+++ ewbapp/ewbappApp.swift
@@ -3,12 +3,16 @@ import SwiftUI
 @main
 struct LamaLamaRangersApp: App {
     @StateObject private var appEnv = AppEnvironment.shared
+    @StateObject private var themeVM = AppThemeViewModel()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(appEnv)
                 .environmentObject(appEnv.authManager)
+                .environmentObject(themeVM)
+                .preferredColorScheme(themeVM.colorScheme)
+                .modifier(RedLightModifier(isActive: themeVM.isRedLightMode))
                 .onAppear {
                     Task {
                         await appEnv.syncEngine.startMonitoring()
